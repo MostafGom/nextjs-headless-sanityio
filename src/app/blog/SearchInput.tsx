30a1b1057196352c140d9bb902bfa38ab26c1ea1
mostafagomaa <mostafagomaa@githubgeneral.com>
2024-03-11 23:20:27 +0200

    edit blog route-try fix production

--- src/app/blog/SearchInput.tsx
+++ src/app/blog/SearchInput.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 // import { getSanitySearchPosts } from './api'; // Assuming you have an API file
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -6,6 +6,8 @@ import { Button } from '@/components/ui/button';
 import PostCard from '@/components/PostCard';
 import PaginationButton from '@/app/blog/PaginationButton';
 
+export const dynamic = 'force-dynamic'
+
 export default async function Page({ searchParams }:
     {
         searchParams:
