0b82af44ef01555e36550334f6e31fdbea554fd0
mostafagomaa <mostafagomaa@githubgeneral.com>
2024-03-14 23:04:29 +0200

    handle laast paginated page

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -3,6 +3,10 @@ import { getSanityPostsPaginatedList } from '@/lib/sanityClient';
 import PostCard from '@/components/PostCard';
 import PaginationButton from '@/components/PaginationButton';
 import { Metadata, ResolvingMetadata } from 'next';
+import { TypographyH2 } from '@/components/Typography';
+import { Button } from '@/components/ui/button';
+import { ArrowLeftCircleIcon } from '@heroicons/react/24/solid'
+import Link from 'next/link';
 
 export const revalidate = 60
 
@@ -39,11 +43,37 @@ export default async function Page({ params, searchParams }:
 
     return (
         <div className='container'>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-12 place-items-center">
-                {posts.map((post: any) => (
-                    <PostCard key={post._id} post={post} />
-                ))}
-            </div>
+            {/* <div className="grid grid-cols-1 md:grid-cols-2 gap-12 place-items-center">
+                {posts.lenght > 0
+                    ? posts.map((post: any) => (
+                        <PostCard key={post._id} post={post} />
+                    ))
+                    : <div className='flex flex-col justify-center items-center w-full'>
+                        <TypographyH2 className='mx-auto text-center' content='More articles are on the way in the meantime you can go visit our Home page' />
+                        <Link href='/'>
+                            <Button className='mt-8'> <ArrowLeftCircleIcon /> Go Back To Home Page</Button>
+                        </Link>
+                    </div>
+                }
+            </div> */}
+
+            {posts.length > 0
+                ? <div className="grid grid-cols-1 md:grid-cols-2 gap-12 place-items-center">
+                    {posts.map((post: any) => (
+                        <PostCard key={post._id} post={post} />
+                    ))}
+                </div>
+
+
+                : <div className='flex flex-col justify-center items-center w-full'>
+                    <TypographyH2 className='mx-auto text-center max-w-3xl' content='More articles are on the way in the meantime you can go visit our Home page' />
+                    <Link href='/'>
+
+                        <Button className='mt-8'> <ArrowLeftCircleIcon className='color-white w-8 h-8' /> Go Back To Home Page</Button>
+                    </Link>
+                    <TypographyH2 className='mx-auto text-center mt-8' content='👇 Or You Can Go To Previous Page From Prev' />
+                </div>
+            }
             <PaginationButton page={page} cursor={lastId} />
         </div>
     );
